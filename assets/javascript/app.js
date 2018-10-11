848b927b8c0b489d16ed6499df0ad20edf26aacc
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2018-10-11 18:22:03 -0500

    Revisit functions by Edgar Reynaga

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
-var athletesArray = ["Michael Jackson", "Aretha Franklin", "Stevie Wonder", "Marvin Gaye", "Bob Marley"];
+var artistArray = ["Michael Jackson", "Aretha Franklin", "Stevie Wonder", "Marvin Gaye", "Bob Marley"];
 
 $(document).ready(function () {
-    for (var i = 0; i < athletesArray.length; i++) {
-        $("#athlete-buttons").append("<button type='button' onclick='searchGif(\"" + athletesArray[i] + "\")' class='btn btn-primary' value=' " + athletesArray[i] + "'> " + athletesArray[i] + " </button>");
+    for (var i = 0; i < artistArray.length; i++) {
+        $("#artist-buttons").append("<button type='button' onclick='searchGif(\"" + artistArray[i] + "\")' class='btn btn-primary' value=' " + artistArray[i] + "'> " + artistArray[i] + " </button>");
     }
 });
 
 function athleteButtonClicked() {
-    var userInput = $('#athlete-input').val();
+    var userInput = $('#artist-input').val();
     searchGif(userInput);
 }
 
 function submitButtonClicked() {
-    var userInput = $('#athlete-input').val();
+    var userInput = $('#artist-input').val();
 
     if (userInput) {
-        $('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+        $('#artist-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
     }
 }
 
@@ -30,7 +30,7 @@ function searchGif(gifName) {
 }
 
 function displayGif(response) {
-    $('#athletes').empty();
+    $('#artists').empty();
     for (var i = 0; i < response.data.length; i++) {
         var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
         var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
@@ -38,7 +38,7 @@ function displayGif(response) {
             ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
 
         image = '<div class="col-md-4">' + image + "</div>";
-        $('#athletes').append(image);
+        $('#artists').append(image);
     }
 
     $('.movImage').on('click', function () {
